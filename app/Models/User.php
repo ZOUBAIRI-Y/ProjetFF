3394ca60aa0040ce8d16da3e916e036d0d712cdd
userdev34 <tom.tanger23@gmail.com>
2024-02-19 10:44:56 +0100

    ajouter les migration et seeders

---
+++ app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+    protected $fillable = ['first_name', 'last_name', 'password', 'email', 'avatar', 'phone', 'role'];
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
