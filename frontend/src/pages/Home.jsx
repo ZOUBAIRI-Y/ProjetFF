ea8aee2a0186a7bce875a9aee4218b0c9c7ae99e
yassin <zobairiyassin@gmail.com>
2024-03-07 12:58:22 +0000

    Add the advantages section in home page, bootstrap icons download

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,64 @@ export default function Home({ ishome }) {
             <div className="advantages_section container mt-5">
                 <h5 className="text-secondary">Our advantages</h5>
                 <h2 className="text-primary">Giving you peace of mind</h2>
+                <ul className="list-group d-flex flex-row justify-content-center ">
+                    <li className="list-group-item">
+                        <i class="bi bi-heart-fill"></i>
+                        <h6>Comfortable</h6>
+                        <p>
+                            Enjoy lifestyle amenities designed to provide every
+                            homeowners modern comfort, a stone's throw away from
+                            schools, churches, and hospitals.
+                        </p>
+                    </li>
+                    <li className="list-group-item">
+                        <i class="bi bi-cash-coin"></i>
+                        <h6>Best price</h6>
+                        <p>
+                            Not sure what you should be charging for your
+                            property? Let us do the numbers for you—contact us
+                            today for a free rental appraisal on your home.
+                        </p>
+                    </li>
+                    <li className="list-group-item">
+                        <i class="bi bi-shield-check"></i>
+                        <h6>Extra security</h6>
+                        <p>
+                            You can connect with potential tenants without
+                            having to share your phone number. We also require
+                            all users to register to validate their legitimacy.
+                        </p>
+                    </li>
+                    <li className="list-group-item">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <h6>Strategic location</h6>
+                        <p>
+                            located in the city center close to the shopping
+                            center. Very good for areas surrounded by
+                            international education centers, start-up office
+                            centers.
+                        </p>
+                    </li>
+                    <li className="list-group-item">
+                        <i class="bi bi-star-fill"></i>
+                        <h6>Luxury</h6>
+                        <p>
+                            Find out how we provide the highest standard of
+                            professional property management to give you all the
+                            benefits of property.
+                        </p>
+                    </li>
+                    <li className="list-group-item">
+                        <i class="bi bi-heart-fill"></i>
+                        <h6>Efficient</h6>
+                        <p>
+                            Schedule visits to multiple properties at once in
+                            one day without having to call them one by one.
+                            Check everything and find the best properties for
+                            rent
+                        </p>
+                    </li>
+                </ul>
             </div>
         </>
     );
