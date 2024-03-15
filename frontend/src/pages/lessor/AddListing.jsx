25bcdf7a5d8d84a8cfed25c9ddaf4c132b9daa09
ZOUBAIRI-Y <zobairiyassin@gmail.com>
2024-03-15 09:07:08 +0000

    commit add listing page before merging

--- frontend/src/pages/lessor/AddListing.jsx
+++ frontend/src/pages/lessor/AddListing.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import LessorSidebar from "../../layouts/LessorSidebar";
+import { useState } from "react";
 
 export default function AddListing() {
+    const [property_inputs, setPropertyinfos] = useState({
+        
+    })
     return (
         <div className="d-flex flex-row justify-content-center">
             <LessorSidebar />
