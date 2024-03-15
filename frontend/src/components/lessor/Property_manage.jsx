05451ba15eccbee4991c43b282c2b8aaecf3d3c4
yassin <zobairiyassin@gmail.com>
2024-03-15 00:07:53 +0000

    update Add listing page

--- frontend/src/components/lessor/Property_manage.jsx
+++ frontend/src/components/lessor/Property_manage.jsx
@@ -7,7 +7,7 @@ function Property_manage() {
             </div>
             <div className="col-md-5 property_quickInfos align-self-center pt-2">
                 {/* <p className="property_address text-dark" name="property_address">Address: Bloc 04 Ait melloul Inezgane</p> */}
-                <h4 className="property_address m-0 text-dark" name="property_address">Address: Bloc 04 Ait melloul Inezgane</h4>
+                <h6 className="property_address m-0 text-dark" name="property_address">Address: Bloc 04 Ait melloul Inezgane</h6>
                 <p className="property_price" name="property_price"><strong className="text-success">Price: 250.00 dh</strong> <span name="rental_type" className="text-light">/day</span></p>
             </div>
             <div className="col-md-4 property_actions d-flex flex-row align-items-center justify-content-center">
--- frontend/src/pages/lessor/AddListing.jsx
+++ frontend/src/pages/lessor/AddListing.jsx
@@ -5,91 +5,93 @@ export default function AddListing() {
     return (
         <div className="d-flex flex-row justify-content-center">
             <LessorSidebar />
-            <div className="container m-0 p-3">
+            <div className="container m-0 pt-5">
                 <div className="next_previous_step"></div>
                 <form className="listing_form p-3">
-                    <p className="text-primary fw-medium m-0">Primary*</p>
-                    <select
-                        name="month_daily_inp"
-                        className="month_daily_inp form-select"
-                    >
-                        <option value="">Monthly/daily</option>
-                        <option value="monthly">Monthly</option>
-                        <option value="daily">Daily</option>
-                    </select>
-                    <select
-                        name="housing_category_inp"
-                        className="housing_category_inp form-select"
-                    >
-                        <option value="apartment">Apartment</option>
-                        <option value="studio">Studio</option>
-                        <option value="front-beach">Front-beach</option>
-                    </select>
-                    <input
-                        type="text"
-                        placeholder="address"
-                        className="address_inp form-control"
-                    />
-                    <select
-                        name="rooms_num_inp"
-                        className="rooms_num_inp form-select"
-                    >
-                        <option value="">? of rooms</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="2">3</option>
-                        <option value="2">2</option>
-                        <option value="2">...</option>
-                    </select>
-                    <select
-                        name="baths_num_inp"
-                        className="baths_num_inp form-select"
-                    >
-                        <option value="">? of baths</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="2">...</option>
-                    </select>
-                    <input
-                        type="number"
-                        name="price_inp"
-                        className="price_inp form-control"
-                        placeholder="Price"
-                    />
-                    <input
-                        type="number"
-                        name="deposit_inp"
-                        className="deposit_inp form-control"
-                        placeholder="Deposit"
-                    />
-                    <input
-                        type="number"
-                        name="space_inp"
-                        className="space_inp form-control"
-                        placeholder="Space"
-                    />
-                    <textarea className="description_inp form-control" />
-                    <input
-                        type="number"
-                        name="phone_inp"
-                        className="phone_inp form-control"
-                        placeholder="Phone"
-                    />
-                    <input
-                        type="file"
-                        name="photos_inp"
-                        className="photos_inp form-control"
-                    />
-                    <input
-                        type="file"
-                        name="video_inp"
-                        className="video_inp form-control"
-                    />
-                    <input
-                        type="date"
-                        name="when_ready_inp"
-                        className="when_ready_inp form-control"
-                    />
+                    <div className="primary_property_infos border rounded pt-1 p-3 bg-altlight">
+                        <p className="text-primary fw-medium m-0">Primary*</p>
+                        <select
+                            name="month_daily_inp"
+                            className="month_daily_inp form-select"
+                        >
+                            <option value="">Monthly/daily</option>
+                            <option value="monthly">Monthly</option>
+                            <option value="daily">Daily</option>
+                        </select>
+                        <select
+                            name="housing_category_inp"
+                            className="housing_category_inp form-select"
+                        >
+                            <option value="apartment">Apartment</option>
+                            <option value="studio">Studio</option>
+                            <option value="front-beach">Front-beach</option>
+                        </select>
+                        <input
+                            type="text"
+                            placeholder="address"
+                            className="address_inp form-control"
+                        />
+                        <select
+                            name="rooms_num_inp"
+                            className="rooms_num_inp form-select"
+                        >
+                            <option value="">? of rooms</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="2">3</option>
+                            <option value="2">2</option>
+                            <option value="2">...</option>
+                        </select>
+                        <select
+                            name="baths_num_inp"
+                            className="baths_num_inp form-select"
+                        >
+                            <option value="">? of baths</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="2">...</option>
+                        </select>
+                        <input
+                            type="number"
+                            name="price_inp"
+                            className="price_inp form-control"
+                            placeholder="Price"
+                        />
+                        <input
+                            type="number"
+                            name="deposit_inp"
+                            className="deposit_inp form-control"
+                            placeholder="Deposit"
+                        />
+                        <input
+                            type="number"
+                            name="space_inp"
+                            className="space_inp form-control"
+                            placeholder="Space"
+                        />
+                        <textarea className="description_inp form-control" />
+                        <input
+                            type="number"
+                            name="phone_inp"
+                            className="phone_inp form-control"
+                            placeholder="Phone"
+                        />
+                        <input
+                            type="file"
+                            name="photos_inp"
+                            className="photos_inp form-control"
+                        />
+                        <input
+                            type="file"
+                            name="video_inp"
+                            className="video_inp form-control"
+                        />
+                        <input
+                            type="date"
+                            name="when_ready_inp"
+                            className="when_ready_inp form-control"
+                        />
+                    </div>
                     <p className="text-primary fw-medium mt-3">
                         Additional features
                     </p>
