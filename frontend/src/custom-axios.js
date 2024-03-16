33981b153a62b7bfd71b43141257b7dcfe8a1b7b
userdev34 <tom.tanger23@gmail.com>
2024-03-16 16:01:51 +0000

    ajoute Login

---
+++ frontend/src/custom-axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const p1 = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+});
+
+// Check if token exists in localStorage
+const token = localStorage.getItem('token');
+if (token) {
+  p1.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+export default p1;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,47 @@
-import Header from "../layouts/Header";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import client from "../custom-axios";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        client
+            .post("http://127.0.0.1:8000/api/login", { email, password })
+            .then((response) => {
+                if (response.status === 200) {
+                    localStorage.setItem("token", response.data.token);
+                    navigate("/");
+                }
+            })
+            .catch((error) => {
+                console.log(error.response.data.errors);
+            });
+    };
+
     return (
-        <>  
-            <div>Hi from Login page</div>
+        <>
+            <form onSubmit={handleSubmit} className="p-4">
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
         </>
-    )
+    );
 }
