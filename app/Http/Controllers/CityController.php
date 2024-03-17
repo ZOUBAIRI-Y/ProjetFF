117631b842c6ffc3c151e70d0b992181c0f7b81c
userdev34 <tom.tanger23@gmail.com>
2024-03-17 12:27:39 +0000

    ajoute route cities

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CityResource;
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $res = CityResource::collection(City::all());
+        return response()->json(["data" => $res], 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->authorize('create');
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $city = City::create($validatedData);
+        $res = new CityResource($city);
+
+        return response()->json($res, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $city = City::find($id);
+
+
+        if (!$city) {
+            return response()->json(['error' => 'City not found'], 404);
+        }
+        $res = new CityResource($city);
+        return response()->json($res);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $city = City::find($id);
+        $this->authorize('update');
+
+        if (!$city) {
+            return response()->json(['error' => 'city not found'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $city->update($validatedData);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $city = City::find($id);
+        $this->authorize('delete');
+        if (!$city) {
+            return response()->json(['error' => 'city not found'], 404);
+        }
+
+
+        $city->delete();
+
+        return response()->json(['message' => 'city deleted']);
+    }
+}
