37390af1f875059afe4c7a4b56240a9f3f191532
userdev34 <tom.tanger23@gmail.com>
2024-03-18 13:08:19 +0000

    ajoute route categories.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = CategoryResource::collection(Category::all());
+        return response()->json(["data" => $categories], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->authorize('create');
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category = Category::create($validatedData);
+        $res = new CategoryResource($category);
+
+        return response()->json($res, 201);
+    }
+
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        $res = new CategoryResource($category);
+        return response()->json($res);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $category = Category::find($id);
+        $this->authorize('update');
+
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category->update($validatedData);
+
+        return response()->json(['message' => 'Category updated']);
+    }
+
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+        $this->authorize('delete');
+
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        $category->delete();
+
+        return response()->json(['message' => 'Category deleted']);
+    }
+}
