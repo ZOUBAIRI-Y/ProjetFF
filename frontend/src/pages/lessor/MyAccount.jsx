95e18f025a2471700cdd560646dd88040b1af1e2
userdev34 <tom.tanger23@gmail.com>
2024-03-26 10:55:43 +0000

    ajoute account update les informations.

--- frontend/src/pages/lessor/MyAccount.jsx
+++ frontend/src/pages/lessor/MyAccount.jsx
@@ -1,122 +1,210 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LessorSidebar from "../../layouts/LessorSidebar";
-import luffy from "../../assets/luffy.jpg"
+import luffy from "../../assets/luffy.jpg";
+import { useNavigate } from "react-router-dom";
+import client from "../../custom-axios";
 
 export default function MyAccount() {
-    const [lessorInfos_inputes, setLessorInfos] = useState({
-        lessor_name: "",
-        lessor_image: "",
-        lessor_firstName: "",
-        lessor_lastName: "",
-        lessor_address: "",
-        lessor_phone1: "",
-        lessor_phone2: "",
-        lessor_email: "",
-        current_password: "",
-        new_password: "",
-        confirm_password: ""
-    })
-    const handleAcc_inputes = (e) => {
-        setLessorInfos({
-            ...lessorInfos_inputes, [e.target.name]: e.target.value
-        })
-    } 
+    const navigate = useNavigate();
+    const [data, setData] = useState({});
+    const [passwordData, setPasswordData] = useState({});
+    const [userData, setUserData] = useState({});
+    useEffect(() => {
+        if (localStorage.getItem("id") === null) {
+            navigate("/login");
+        }
+        const id = localStorage.getItem("id");
+        client
+            .get("http://localhost:8000/api/users/" + id)
+            .then(({ data }) => {
+                setData(data.data);
+            })
+            .catch((err) => console.log(err.response.data));
+    }, []);
+
+    const saveData = (e) => {
+        e.preventDefault();
+        console.log(userData);
+        client
+            .patch(
+                "http://localhost:8000/api/users/" + localStorage.getItem("id"),
+                userData
+            )
+            .then(({ data }) => {
+                console.log(data.data);
+            })
+            .catch((err) => console.log(err.response.data));
+    };
+    const updatePassword = (e) => {};
+
     return (
         <div className="d-flex flex-row justify-content-center">
             <LessorSidebar />
             <div className="container m-0 pt-5">
                 <h2 className="text-primary d-inline">My Account</h2>
-                <button type="button" className="btn btn-success save_changes_btn text-white float-end">
-                    save
-                </button>
-                <form className="account_info mt-3">
-                    <div className="personal_infos border rounded pt-1 p-3 bg-altlight">
-                        <div className="row m-0 mt-2">
-                            <div className="col-sm lessor_name_myAcc ps-0">
-                                <p className="text-dark m-0 "><strong>Lessor name</strong></p>
+                <form className="account_info mt-3" onSubmit={saveData}>
+                    {data && (
+                        <div className="personal_infos border rounded pt-1 p-3 bg-altlight">
+                            <input
+                                value="save"
+                                type="submit"
+                                className="btn m-4 btn-success save_changes_btn text-white float-end"
+                            />
+                            <div className="row m-0 mt-2">
+                                <div className="col-sm lessor_name_myAcc ps-0">
+                                    <p className="text-dark m-0 ">
+                                        <strong>Lessor name</strong>
+                                    </p>
+                                    <input
+                                        type="text"
+                                        name="lessor_name"
+                                        value={userData.name}
+                                        onChange={(e) =>
+                                            setUserData({
+                                                ...userData,
+                                                name: e.target.value,
+                                            })
+                                        }
+                                        className="form-control"
+                                        placeholder={
+                                            data.name ? data.name : "name"
+                                        }
+                                    />
+                                </div>
+                                <div className="col-sm-3 lessor_img_container p-0">
+                                    <img src={luffy} alt="profilePic" />
+                                </div>
+                            </div>
+                            <p className="text-dark mt-1 mb-1">
+                                <strong>Personal informations</strong>
+                            </p>
+                            <div className="lessor_firstName pe-1">
+                                <label htmlFor="lessorfirstName">
+                                    first name
+                                </label>
                                 <input
                                     type="text"
-                                    name="lessor_name"
-                                    value={lessorInfos_inputes.lessor_name}
-                                    onChange={e=> handleAcc_inputes(e)}
+                                    name="lessor_firstName"
+                                    value={userData.firstname}
                                     className="form-control"
-                                    placeholder="lessor name"
+                                    onChange={(e) =>
+                                        setUserData({
+                                            ...userData,
+                                            firstname: e.target.value,
+                                        })
+                                    }
+                                    placeholder={
+                                        data.firstname
+                                            ? data.firstname
+                                            : "firstname"
+                                    }
                                 />
                             </div>
-                            <div className="col-sm-3 lessor_img_container p-0">
-                                <img src={luffy} alt="profilePic" />
+                            <div className="lessor_lastName ps-1">
+                                <label htmlFor="lessorlastName">
+                                    last name
+                                </label>
+                                <input
+                                    type="text"
+                                    name="lessor_lastName"
+                                    value={userData.lastname}
+                                    className="form-control"
+                                    onChange={(e) =>
+                                        setUserData({
+                                            ...userData,
+                                            lastname: e.target.value,
+                                        })
+                                    }
+                                    placeholder={
+                                        data.lastname
+                                            ? data.lastname
+                                            : "lastname"
+                                    }
+                                />
                             </div>
-                        </div>
-                        <p className="text-dark mt-1 mb-1">
-                            <strong>Personal informations</strong>
-                        </p>
-                        <div className="lessor_firstName pe-1">
-                            <label htmlFor="lessorfirstName">first name</label>
+                            <label htmlFor="lessorAddress" className="mt-1">
+                                Address
+                            </label>
                             <input
                                 type="text"
-                                name="lessor_firstName"
-                                value={lessorInfos_inputes.first_name}
+                                name="lessor_address"
+                                value={userData.address}
                                 className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
-                                placeholder="first name"
+                                onChange={(e) =>
+                                    setUserData({
+                                        ...userData,
+                                        address: e.target.value,
+                                    })
+                                }
+                                placeholder={
+                                    data.address ? data.address : "firstname"
+                                }
                             />
-                        </div>
-                        <div className="lessor_lastName ps-1">
-                            <label htmlFor="lessorlastName">last name</label>
+                            <div className="lessor_phone1 mt-1 pe-1">
+                                <label htmlFor="lessorphone1">Phone 1</label>
+                                <input
+                                    type="text"
+                                    name="lessor_phone1"
+                                    value={userData.phone1}
+                                    className="form-control"
+                                    maxLength={10}
+                                    onChange={(e) =>
+                                        setUserData({
+                                            ...userData,
+                                            phone1: e.target.value,
+                                        })
+                                    }
+                                    placeholder={
+                                        data.phone1 ? data.phone1 : "phone"
+                                    }
+                                />
+                            </div>
+                            <div className="lessor_phone2 mt-1 ps-1">
+                                <label htmlFor="lessorphone2">Phone 2</label>
+                                <input
+                                    type="text"
+                                    name="lessor_phone2"
+                                    value={userData.phone2}
+                                    className="form-control"
+                                    maxLength={10}
+                                    onChange={(e) =>
+                                        setUserData({
+                                            ...userData,
+                                            phone2: e.target.value,
+                                        })
+                                    }
+                                    placeholder={
+                                        data.phone2 ? data.phone2 : "phone"
+                                    }
+                                />
+                            </div>
+                            <label htmlFor="lessorEmail" className="mt-1">
+                                Email
+                            </label>
                             <input
                                 type="text"
-                                name="lessor_lastName"
-                                value={lessorInfos_inputes.last_name}
+                                name="lessor_email"
+                                value={userData.email}
                                 className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
-                                placeholder="last name"
+                                onChange={(e) =>
+                                    setUserData({
+                                        ...userData,
+                                        email: e.target.value,
+                                    })
+                                }
+                                placeholder={
+                                    data.email ? data.email : "firstname"
+                                }
                             />
+
+                            <div></div>
                         </div>
-                        <label htmlFor="lessorAddress" className="mt-1">
-                            Address
-                        </label>
-                        <input
-                            type="text"
-                            name="lessor_address"
-                            value={lessorInfos_inputes.address}
-                            className="form-control"
-                            onChange={e=> handleAcc_inputes(e)}
-                            placeholder="Address"
-                        />
-                        <div className="lessor_phone1 mt-1 pe-1">
-                            <label htmlFor="lessorphone1">Phone 1</label>
-                            <input
-                                type="text"
-                                name="lessor_phone1"
-                                value={lessorInfos_inputes.phone1}
-                                className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
-                                placeholder="phone 1"
-                            />
-                        </div>
-                        <div className="lessor_phone2 mt-1 ps-1">
-                            <label htmlFor="lessorphone2">Phone 2</label>
-                            <input
-                                type="text"
-                                name="lessor_phone2"
-                                value={lessorInfos_inputes.phone2}
-                                className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
-                                placeholder="phone 2"
-                            />
-                        </div>
-                        <label htmlFor="lessorEmail" className="mt-1">
-                            Email
-                        </label>
-                        <input
-                            type="text"
-                            name="lessor_email"
-                            value={lessorInfos_inputes.email}
-                            className="form-control"
-                            onChange={e=> handleAcc_inputes(e)}
-                            placeholder="email"
-                        />
-                    </div>
+                    )}
+                </form>
+                <form
+                    className="account_info mt-3 mb-4"
+                    onSubmit={updatePassword}
+                >
                     <div className="change_password border rounded bg-altlight mt-2 pt-1 p-3">
                         <p className="text-dark mt-1">
                             <strong>Change password</strong>
@@ -127,9 +215,14 @@ export default function MyAccount() {
                         <input
                             type="password"
                             name="current_password"
-                            value={lessorInfos_inputes.current_password}
+                            value={""}
                             className="form-control current_password"
-                            onChange={e=> handleAcc_inputes(e)}
+                            onChange={(e) =>
+                                setPasswordData({
+                                    ...passwordData,
+                                    password: e.target.value,
+                                })
+                            }
                             placeholder="current password"
                         />
                         <div className="new_password mt-1 pe-1">
@@ -137,9 +230,14 @@ export default function MyAccount() {
                             <input
                                 type="password"
                                 name="new_password"
-                                value={lessorInfos_inputes.new_password}
+                                value={""}
                                 className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
+                                onChange={(e) =>
+                                    setPasswordData({
+                                        ...passwordData,
+                                        newPassword: e.target.value,
+                                    })
+                                }
                                 placeholder="new password"
                             />
                         </div>
@@ -150,12 +248,22 @@ export default function MyAccount() {
                             <input
                                 type="password"
                                 name="confirm_password"
-                                value={lessorInfos_inputes.confirm_password}
+                                value={""}
                                 className="form-control"
-                                onChange={e=> handleAcc_inputes(e)}
+                                onChange={(e) =>
+                                    setPasswordData({
+                                        ...passwordData,
+                                        password_confirmation: e.target.value,
+                                    })
+                                }
                                 placeholder="confirm password"
                             />
                         </div>
+                        <input
+                            value="change"
+                            type="submit"
+                            className="m-4  btn btn-success save_changes_btn text-white float-end"
+                        />
                     </div>
                 </form>
             </div>
