5082885a6a306e3977340738c6da82e99527e75d
userdev34 <tom.tanger23@gmail.com>
2024-05-08 09:24:19 +0100

    ajoute filter react.

--- frontend/src/pages/PropertiesList.jsx
+++ frontend/src/pages/PropertiesList.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
 import Property from "../components/Property";
-import FormPropertiesList from "../components/propertiesList/FormPropertiesList";
 import client from "../custom-axios";
 import { useParams } from "react-router-dom";
 
 export default function PropertiesList() {
     const { term } = useParams();
     const [list, setList] = useState([]);
-    // const navigate = useNavigate();
+    const [terms, setTerms] = useState(term);
+    const [selectedCity, setSelectedCity] = useState("");
+    const [selectedPrice, setSelectedPrice] = useState("");
+    const [selectedRooms, setSelectedRooms] = useState("");
+
+    const [cities, setCities] = useState([]);
+    useEffect(() => {
+        client
+            .get("http://127.0.0.1:8000/api/cities")
+            .then(({ data }) => {
+                setCities(data.data);
+                console.log(data);
+            })
+
+            .catch((err) => console.log(err));
+    }, []);
 
     useEffect(() => {
-        if (term === "all") {
+        if (term === "all" || !terms) {
             client
                 .get("http://127.0.0.1:8000/api/properties")
                 .then(({ data }) => {
@@ -21,7 +35,7 @@ export default function PropertiesList() {
                 .catch((err) => console.log(err));
         } else {
             client
-                .get("http://127.0.0.1:8000/api/properties/results/" + term)
+                .get("http://127.0.0.1:8000/api/properties/results/" + terms)
                 .then(({ data }) => {
                     console.log(data);
                     setList(data.data);
@@ -29,14 +43,87 @@ export default function PropertiesList() {
 
                 .catch((err) => console.log(err));
         }
-    }, [term]);
+    }, [terms]);
+
+    useEffect(() => {
+        client
+            .get(
+                `http://127.0.0.1:8000/api/properties?rooms[gte]${selectedRooms}&price[lte]${selectedPrice}&city[eq]${selectedCity}`
+            )
+            .then(({ data }) => {
+                setList(data.data);
+            })
+            .catch((err) => console.log(err));
+    }, [selectedCity, selectedPrice, selectedRooms]);
 
     return (
         <>
-            <FormPropertiesList />
+            <form className="container mt-4">
+                <div className="row">
+                    <div className="col-sm-4 ps-0 pe-1">
+                        <input
+                            type="text"
+                            placeholder="searche ..."
+                            className="form-control p-2 "
+                            value={terms}
+                            onChange={(e) => setTerms(e.target.value)}
+                        />
+                    </div>
+                    <div className="col-sm p-0 pe-1">
+                        <select
+                            className="form-select p-2"
+                            aria-label="Default select example"
+                            onChange={(e) => setSelectedRooms(e.target.value)}
+                        >
+                            <option selected="">rooms</option>
+                            <option value={1}>One</option>
+                            <option value={2}>Two</option>
+                            <option value={3}>Three</option>
+                            <option value={4}>Four</option>
+                            <option value={"more"}>More...</option>
+                        </select>
+                    </div>
+                    <div className="col-sm p-0 pe-1">
+                        <select
+                            className="form-select p-2"
+                            aria-label="Default select example"
+                            onChange={(e) => setSelectedCity(e.target.value)}
+                        >
+                            <option selected="">cities</option>
+                            {cities &&
+                                cities.map((c) => (
+                                    <option key={c.id} value={c.name}>
+                                        {c.name}
+                                    </option>
+                                ))}
+                        </select>
+                    </div>
+                    <div className="col-sm p-0 pe-1">
+                        <select
+                            className="form-select p-2"
+                            aria-label="Default select example"
+                            onChange={(e) => setSelectedPrice(e.target.value)}
+                        >
+                            <option selected="">price</option>
+                            <option value={1000}>100</option>
+                            <option value={2000}>200</option>
+                            <option value={3000}>300</option>
+                            <option value={4000}>More...</option>
+                        </select>
+                    </div>
+
+                    <button
+                        type="button"
+                        className="btn btn-outline-success col-sm-2"
+                    >
+                        Advanced
+                    </button>
+                </div>
+            </form>{" "}
             <div className="total_sortProperties_bar container bg-altlight mt-3">
-                <p className="float-start text-primary fw-medium">
-                    ? properties
+                <p className="float-start text-primary fw-medium m-2">
+                    {" "}
+                    properties
                 </p>
 
                 <select
