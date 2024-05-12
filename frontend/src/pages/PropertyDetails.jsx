92edce267d20ac6026c538bc58a7c8145dbdef69
yassin <zobairiyassin@gmail.com>
2024-05-12 20:54:15 +0000

    add property images in ./assets, modification on index.css, propertyDetails

--- frontend/src/pages/PropertyDetails.jsx
+++ frontend/src/pages/PropertyDetails.jsx
@@ -1,9 +1,16 @@
 export default function PropertyDetails () {
     return (
         <>
-            <div className="carousel_propDetails">
+            <section className="prop_gen_infos">
+                <div className="carousel_propDetail">
 
-            </div>
+                </div>
+                <div className="container">
+                    <h2>Apartment</h2>
+                    <p>24 Rue Abbas Lamsaadi, Agadir,Morocco</p>
+                    <p><strong>400,00DH</strong><span>/day</span></p>
+                </div>
+            </section>
             
         </>
     )
